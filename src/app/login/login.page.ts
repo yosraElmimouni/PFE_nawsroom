8cd2ecd3376c5f155d3fdb90f6d3242abb141a2e
yosraElmimouni <161881708+yosraElmimouni@users.noreply.github.com>
2026-04-18 17:09:35 +0100

    initialisation page login with microsoft

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false,
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
