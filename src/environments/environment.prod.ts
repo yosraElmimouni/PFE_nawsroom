19b1edeb6002f2a7d55aab9884d6475f4ea2eb92
YosraElmimouni <yosraelmimouni2003@gmail.com>
2026-05-08 12:19:51 +0200

    api cache

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,8 @@
 export const environment = {
   production: true,
 
-  geminiApiKey: 'AIzaSyC0p0LlCJMKXHr9gdnjru94ZKA_dzVLH4U',
+  // geminiApiKey: ,
 
-  msal: {
-    clientId: 'f5515d18-8765-4ae6-8b08-2b4b8ad66611',
-    authority: 'https://login.microsoftonline.com/dc59e38c-4977-406f-bdd1-9ebbabbd387e',
-    webRedirectUri: 'https://newsroom-ai.yourdomain.com',
-    nativeRedirectUri: 'msauth://ma.ac.usms.newsroom/auth',
-  },
+  // 
+  // },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
 
-  geminiApiKey: 'AIzaSyC0p0LlCJMKXHr9gdnjru94ZKA_dzVLH4U',
+  // geminiApiKey: '',
 
-  msal: {
-    clientId: 'f5515d18-8765-4ae6-8b08-2b4b8ad66611',
-    authority: 'https://login.microsoftonline.com/dc59e38c-4977-406f-bdd1-9ebbabbd387e',
-    webRedirectUri: 'http://localhost:8100',
-    nativeRedirectUri: 'msauth://ma.ac.usms.newsroom/auth',
-  },
+  // msal: {
+  //   clientId: '',
+  //   authority: 'https://login.microsoftonline.com/',
+  //   webRedirectUri: 'http://localhost:8100',
+  //   nativeRedirectUri: 'msauth://ma.ac.usms.newsroom/auth',
+  // },
 };
 
 
